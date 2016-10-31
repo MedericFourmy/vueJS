9f52fcb224a1a66be8e1fdf0ef47deea15fce7cb
Mederic <mederic@Admins-MBP.fios-router.home>
2016-10-31 11:38:20 -0400

    list rendering intro

---
+++ official_tuto/listRendering/app.js
@@ -0,0 +1,20 @@
+var app1 = new Vue({
+	el: "#app1",
+	data: {
+		items: [
+		  { message: 'Foo' },
+		  { message: 'Bar' }
+		]
+	}
+});
+
+var example2 = new Vue({
+  el: '#example-2',
+  data: {
+    parentMessage: 'Parent',
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
